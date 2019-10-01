48c9f517b6a4122ca88889a304b27ef0602b5453
William Atwood <46944152+Attigator1@users.noreply.github.com>
2019-10-01 15:12:43 -0600

    Update wa223_port_scanner.py
    Added SP functions and such

--- wa223_port_scanner.py
+++ wa223_port_scanner.py
@@ -1,5 +1,7 @@
 #! /usr/bin/python
 
+import sys
+import time
 import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
@@ -16,19 +18,32 @@ menu = input("Enter value: ")
 if menu == 1
   print ("---TCP Scan--- \n")
   print ("Please list the hostnames you would like to test. Separate each hostname with a ';' with NO spaces. Here is an example: 12.122.127.89;137.89.57.18;www.google.com;0.0.0.0\n")
-  TCPHinput = input(">")
+  TCPHinput = input("->")
   TCPHostList = TCPHinput.split(";")
   print len(TCPHostList)
   for i in TCPHostList
-    print (Selected Hostname/IP: " + TCPHostList[i] + "\n")
+    print ("Selected Hostname/IP: " + TCPHostList[i] + "\n")
     print ("For this IP, please list the ports you want to test. Same as last time, separate the host with a ';' with NO spaces. Here is an example: 88;89;144\n")
-    TCPPortinput = input(">")
+    TCPPortinput = input("->")
     TCPPortList = TCPPortinput.split(";")
     for p in TCPPortList
       print ("Testing port: " + TCPPortList[p])
       if (TCPPortList[p].isdigit())
         #do Scapy stuff here
         # add print function here
+        dst_ip = TCPHostList[i]
+        src_port = RandShort()
+        dst_port= TCPPortList[p]
+
+        tcp_connect_scan_resp = sr1(IP(dst=dst_ip)/TCP(sport=src_port,dport=dst_port,flags="S"),timeout=10)
+        if(str(type(tcp_connect_scan_resp))=="<type 'NoneType'>"):
+            print ("Port:" + TCPPortList[p] + "Closed")
+        elif(tcp_connect_scan_resp.haslayer(TCP)):
+            if(tcp_connect_scan_resp.getlayer(TCP).flags == 0x12):
+                send_rst = sr(IP(dst=dst_ip)/TCP(sport=src_port,dport=dst_port,flags="AR"),timeout=10)
+                print ("Port:" + TCPPortList[p] + "Open")
+            elif (tcp_connect_scan_resp.getlayer(TCP).flags == 0x14):
+                print ("Port:" + TCPPortList[p] + " Closed"
       else
         print ("Not a valid port value! Moving on to the next port in list")
 
@@ -39,7 +54,7 @@ if menu == 1
 
 
 elif menu == 2
-  print "---UDP Scan--- \n"
+  print ("---UDP Scan--- \n")
   print ("Please list the hostnames you would like to test. Separate each hostname with a ';' with NO spaces. Here is an example: 12.122.127.89;137.89.57.18;www.google.com;0.0.0.0\n")
   UDPHinput = input(">")
   UDPHostList = UDPHinput.split(";")
@@ -54,11 +69,32 @@ elif menu == 2
       if (UDPPortList[p].isdigit())
         #do Scapy stuff here
         # add print function here
+        dst_ip = UDPHostList[i]
+        src_port = RandShort()
+        dst_port= UDPPortList[p]
+
+
+
+
+        pkt = sr1(IP(dst=target)/UDP(sport=src_port, dport=dst_port), timeout=2, verbose=0)
+		if(str(type(pkt))=="<type 'NoneType'>"):
+            print ("Port:" + dst_port + "Closed")
+		else:
+			if pkt.haslayer(ICMP):
+				print ("Port:" + dst_port + "Closed")
+			elif pkt.haslayer(UDP):
+				print ("Port:" + dst_port + "Open / filtered")
+			else:
+				print ("Port:" + dst_port + "Unknown")
+				print(pkt.summary())
+
+
+
       else
         print ("Not a valid port value! Moving on to the next port in list")
 
 elif menu == 3
-  print "---Traceroute--- \n"
+  print ("---Traceroute--- \n")
   TInput = input("Enter hostname or IP: ")
   for i in range(1, 30):
     Tpkt = IP(dst=TInput, ttl=i) / UDP(dport=33434)
@@ -68,19 +104,19 @@ elif menu == 3
       break
     elif reply.type == 3:
       # we hit it
-      print "Done!", reply.src
+      print ("Done!\n" + reply.src)
       break
     else:
       # keep going
-      print "%d hops away: " % i , reply.src
+      print ("%d hops away: " % i , reply.src)
 
 
 
 
 elif menu == 4
-  print "---Ending Program--- \n"
-  print "Goodbye!"
+  print ("---Ending Program--- \n")
+  print ("Goodbye!")
   sys.exit()
 
 else
-  print "NOT A VAILD INPUT!! TRY AGAIN! \n"
+  print ("NOT A VAILD INPUT!! TRY AGAIN! \n")
